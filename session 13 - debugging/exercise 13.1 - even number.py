a1b964433a45dfa12c057a80075d0e5c6d2d54e8
Rizky Permadi Syah <rizkypermadisyah@gmail.com>
2023-02-21 21:44:19 +0700

    complete all session 13 lessons and exercises

---
+++ session 13 - debugging/exercise 13.1 - even number.py
@@ -0,0 +1,15 @@
+# bug
+# number = int(input("Which number do you want to check?"))
+#
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+# debug
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:  # change = to ==
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
---
+++ session 13 - debugging/exercise 13.2 - leap year.py
@@ -0,0 +1,28 @@
+# bug
+# year = input("Which year do you want to check?")
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+
+
+# debug
+year = int(input("Which year do you want to check?"))  # move input to int()
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
